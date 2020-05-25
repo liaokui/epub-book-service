0fd9811c0efe09eca7477e1e6ce9de9176b47f4f
liaokui <13047903316lelk@gmail.com>
2020-05-26 01:38:59 +0800

    修改配置

--- config/config.default.js
+++ config/config.default.js
@@ -41,8 +41,8 @@ module.exports = appInfo => {
   const mongoose = {
     clients: {
       epubbook: {
-        // url: 'mongodb://127.0.0.1:27017/epubbook',
-        url: 'mongodb://150.109.105.237:27017/epubbook',
+        url: 'mongodb://127.0.0.1:27017/epubbook',
+        // url: 'mongodb://150.109.105.237:27017/epubbook',
         options: {
           user: 'epubbookUser', // 数据库账号
           pass: '123456', // 数据库密码
